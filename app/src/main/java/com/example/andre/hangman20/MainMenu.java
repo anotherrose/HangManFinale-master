d738c3b8ba857f7a246b6453c5ef8f56f416c97e
anotherrose98 <andrewmwilson98@gmail.com>
2017-03-10 13:01:35 -0500

    seven letter game screen and three letter gmae screen are not working

---
+++ app/src/main/java/com/example/andre/hangman20/MainMenu.java
@@ -0,0 +1,37 @@
+package com.example.andre.hangman20;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menue);
+
+        Button easy = (Button) findViewById(R.id.btnEasy);
+        Button medium = (Button) findViewById(R.id.btnMedium);
+        Button hard = (Button) findViewById(R.id.btnHardest);
+
+
+    }
+
+    public void StartEasy(View view) {
+        Intent i = new Intent(this, FiveWordGame.class);
+        startActivity(i);
+    }
+
+    public void StartMedium(View view) {
+        Intent i = new Intent(this, SevenWordGame.class);
+        startActivity(i);
+    }
+
+    public void StartHard(View view) {
+        Intent i = new Intent(this, ThreeWordGame.class);
+        startActivity(i);
+    }
+}
--- app/src/main/java/com/example/andre/hangman20/SplashScreen.java
+++ app/src/main/java/com/example/andre/hangman20/SplashScreen.java
@@ -2,25 +2,33 @@ package com.example.andre.hangman20;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class SplashScreen extends AppCompatActivity {
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable(){
+        @Override
+        public void run() {
+            closeSplash();
+            //handler.postDelayed(runnable, 1000);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
 
-        Button btnSwitch = (Button) findViewById(R.id.btnPlay);
-        btnSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myInt = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(myInt);
-            }
-        });
+        handler.postDelayed(runnable,5000);
 
+
+    }
+
+    private void closeSplash() {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 }
